9fffa6cad640686dca26e901ff8953bc082bcf5a
Martin Sjölund <martin.sjolund@liu.se>
2020-04-30 13:26:20 +0200

    3.2.3 version of the update script

--- update.sh
+++ update.sh
@@ -1,19 +1,20 @@
 #!/bin/sh -xe
 
-MASHA1=`git rev-parse MA/master`
+MASHA1=`git rev-parse MA/maint/3.2.3`
 if git log -l1 | grep -q "$MASHA1"; then
   exit 0
 fi
 
-for dir in Modelica ModelicaReference ModelicaServices ModelicaTest ModelicaTestConversion4.mo ModelicaTestOverdetermined.mo ObsoleteModelica4.mo Complex.mo; do
+for dir in Modelica ModelicaReference ModelicaServices ModelicaTest ModelicaTestOverdetermined.mo ObsoleteModelica3.mo Complex.mo; do
   rm -rf "$dir"
-  git checkout MA/master "$dir"
+  git checkout MA/maint/3.2.3 "$dir"
   git add "$dir"
 done
 rm -rf Modelica/Resources/Library
+patch --ignore-whitespace -p1 -f < Modelica.patch
 patch --ignore-whitespace -p1 -f < ModelicaServices.patch
 docker run -w "$PWD" -v "$PWD:$PWD" openmodelica/openmodelica:v1.14.1-minimal omc patch.mos
 git config --global user.email "openmodelica@ida.liu.se"
 git config --global user.name "OpenModelica Jenkins"
 git commit -a -m "Update to $MASHA1"
-git push github OM/master
+git push github OM/maint/3.2.3
